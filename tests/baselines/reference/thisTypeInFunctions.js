22af809755d19742ad0f05a0ddb738f6d749279f
Nathan Shively-Sanders <nathansa@microsoft.com>
2015-11-30 10:04:21 -0800

    Improve test and add baselines
    1. Change to meaningful names.
    2. Add noThis vs voidThis cases.

--- tests/baselines/reference/thisTypeInFunctions.js
+++ tests/baselines/reference/thisTypeInFunctions.js
@@ -1,33 +1,49 @@
 //// [thisTypeInFunctions.ts]
+function propertyName(this: { y: number }, x: number): number {
+    return x + this.notFound;
+}
 function f(this: { y: number }, x: number): number {
     return x + this.y;
 }
-function g(this: { y: number }, x: number): number {
-    return x + this.notFound;
+function noThisSpecified(x: number): number {
+    // for backward compatibility, this: any, so this is ok
+    // (until we add --noImplicitThisAny)
+    return x + this.notSpecified;
 }
-let obj: {y: number, f: (this: { y: number }, x: number) => number} = { y: 12, f };
-let obj2: {y: string, f: (this: { y: number }, x: number) => number} = { y: 'foo', f };
-let obj3: {wrongName: number, f: (this: { y: number }, x: number) => number} = { wrongName: 12, f };
+function voidThisSpecified(this: void, x: number): number {
+    return x + this.notSpecified;
+}
+let ok: {y: number, f: (this: { y: number }, x: number) => number} = { y: 12, f };
+let wrongPropertyType: {y: string, f: (this: { y: number }, x: number) => number} = { y: 'foo', f };
+let wrongPropertyName: {wrongName: number, f: (this: { y: number }, x: number) => number} = { wrongName: 12, f };
 
-obj.f(13, 'nope');
-obj.f(13);
-obj.f('nope');
-obj2.f(13);
-obj3.f(13);
+ok.f(13);
+ok.f('wrong type');
+ok.f(13, 'too many arguments');
+wrongPropertyType.f(13);
+wrongPropertyName.f(13);
 
 
 //// [thisTypeInFunctions.js]
+function propertyName(this, x) {
+    return x + this.notFound;
+}
 function f(this, x) {
     return x + this.y;
 }
-function g(this, x) {
-    return x + this.notFound;
+function noThisSpecified(x) {
+    // for backward compatibility, this: any, so this is ok
+    // (until we add --noImplicitThisAny)
+    return x + this.notSpecified;
 }
-var obj = { y: 12, f: f };
-var obj2 = { y: 'foo', f: f };
-var obj3 = { wrongName: 12, f: f };
-obj.f(13, 'nope');
-obj.f(13);
-obj.f('nope');
-obj2.f(13);
-obj3.f(13);
+function voidThisSpecified(this, x) {
+    return x + this.notSpecified;
+}
+var ok = { y: 12, f: f };
+var wrongPropertyType = { y: 'foo', f: f };
+var wrongPropertyName = { wrongName: 12, f: f };
+ok.f(13);
+ok.f('wrong type');
+ok.f(13, 'too many arguments');
+wrongPropertyType.f(13);
+wrongPropertyName.f(13);
--- tests/cases/conformance/types/thisType/thisTypeInFunctions.ts
+++ tests/cases/conformance/types/thisType/thisTypeInFunctions.ts
@@ -1,15 +1,23 @@
+function propertyName(this: { y: number }, x: number): number {
+    return x + this.notFound;
+}
 function f(this: { y: number }, x: number): number {
     return x + this.y;
 }
-function g(this: { y: number }, x: number): number {
-    return x + this.notFound;
+function noThisSpecified(x: number): number {
+    // for backward compatibility, this: any, so this is ok
+    // (until we add --noImplicitThisAny)
+    return x + this.notSpecified;
 }
-let obj: {y: number, f: (this: { y: number }, x: number) => number} = { y: 12, f };
-let obj2: {y: string, f: (this: { y: number }, x: number) => number} = { y: 'foo', f };
-let obj3: {wrongName: number, f: (this: { y: number }, x: number) => number} = { wrongName: 12, f };
+function voidThisSpecified(this: void, x: number): number {
+    return x + this.notSpecified;
+}
+let ok: {y: number, f: (this: { y: number }, x: number) => number} = { y: 12, f };
+let wrongPropertyType: {y: string, f: (this: { y: number }, x: number) => number} = { y: 'foo', f };
+let wrongPropertyName: {wrongName: number, f: (this: { y: number }, x: number) => number} = { wrongName: 12, f };
 
-obj.f(13, 'nope');
-obj.f(13);
-obj.f('nope');
-obj2.f(13);
-obj3.f(13);
+ok.f(13);
+ok.f('wrong type');
+ok.f(13, 'too many arguments');
+wrongPropertyType.f(13);
+wrongPropertyName.f(13);
