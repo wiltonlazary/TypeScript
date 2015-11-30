d7ccb2c1d362a799386cc95b498dd16ec619c2e2
Nathan Shively-Sanders <nathansa@microsoft.com>
2015-11-30 09:31:10 -0800

    Add negative tests and accept baselines
    Not all are passing yet.

--- tests/baselines/reference/thisTypeInFunctions.js
+++ tests/baselines/reference/thisTypeInFunctions.js
@@ -2,13 +2,32 @@
 function f(this: { y: number }, x: number): number {
     return x + this.y;
 }
+function g(this: { y: number }, x: number): number {
+    return x + this.notFound;
+}
 let obj: {y: number, f: (this: { y: number }, x: number) => number} = { y: 12, f };
+let obj2: {y: string, f: (this: { y: number }, x: number) => number} = { y: 'foo', f };
+let obj3: {wrongName: number, f: (this: { y: number }, x: number) => number} = { wrongName: 12, f };
 
-obj.f(13); // 25!!!!
+obj.f(13, 'nope');
+obj.f(13);
+obj.f('nope');
+obj2.f(13);
+obj3.f(13);
+
 
 //// [thisTypeInFunctions.js]
 function f(this, x) {
     return x + this.y;
 }
+function g(this, x) {
+    return x + this.notFound;
+}
 var obj = { y: 12, f: f };
-obj.f(13); // 25!!!!
+var obj2 = { y: 'foo', f: f };
+var obj3 = { wrongName: 12, f: f };
+obj.f(13, 'nope');
+obj.f(13);
+obj.f('nope');
+obj2.f(13);
+obj3.f(13);
--- tests/cases/conformance/types/thisType/thisTypeInFunctions.ts
+++ tests/cases/conformance/types/thisType/thisTypeInFunctions.ts
@@ -1,6 +1,15 @@
 function f(this: { y: number }, x: number): number {
     return x + this.y;
 }
+function g(this: { y: number }, x: number): number {
+    return x + this.notFound;
+}
 let obj: {y: number, f: (this: { y: number }, x: number) => number} = { y: 12, f };
+let obj2: {y: string, f: (this: { y: number }, x: number) => number} = { y: 'foo', f };
+let obj3: {wrongName: number, f: (this: { y: number }, x: number) => number} = { wrongName: 12, f };
 
-obj.f(13); // 25!!!!
+obj.f(13, 'nope');
+obj.f(13);
+obj.f('nope');
+obj2.f(13);
+obj3.f(13);
