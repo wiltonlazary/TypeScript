75c5e52ebf9d38b2bacbf169b16fa394ee40afa0
Nathan Shively-Sanders <nathansa@microsoft.com>
2015-11-30 16:22:03 -0800

    Add assignability tests

--- tests/cases/conformance/types/thisType/thisTypeInFunctions.ts
+++ tests/cases/conformance/types/thisType/thisTypeInFunctions.ts
@@ -1,3 +1,4 @@
+// body checking
 function f(this: { y: number }, x: number): number {
     return x + this.y;
 }
@@ -6,8 +7,19 @@ function noThisSpecified(x: number): number {
     // (until we add --noImplicitThisAny)
     return x + this.notSpecified;
 }
+
+// parameter checking
 let ok: {y: number, f: (this: { y: number }, x: number) => number} = { y: 12, f };
 let implicitAnyOk: {notSpecified: number, f: (x: number) => number} = { notSpecified: 12, f: noThisSpecified };
 ok.f(13);
 noThisSpecified(12);
 implicitAnyOk.f(12);
+
+// assignment checking
+let specifiedToAny: (x: number) => number = f;
+let specifiedToSpecified: (this: {y: number}, x: number) => number = f;
+let anyToSpecified: (this: { y: number }, x: number) => number = function(x: number): number { return x + 12; };
+
+let unspecifiedLambda: (x: number) => number = x => x + 12;
+let specifiedLambda: (this: void, x: number) => number = x => x + 12;
+let unspecifiedLambdaToSpecified: (this: {y: number}, x: number) => number = unspecifiedLambda;
--- tests/cases/conformance/types/thisType/thisTypeInFunctionsNegative.ts
+++ tests/cases/conformance/types/thisType/thisTypeInFunctionsNegative.ts
@@ -16,3 +16,8 @@ ok.f('wrong type');
 ok.f(13, 'too many arguments');
 wrongPropertyType.f(13);
 wrongPropertyName.f(13);
+
+// oops, this triggers contextual typing, which needs to be updated to understand that =>'s `this` is void.
+let voidToSpecified: (this: { y: number }, x: number) => number = x => x + this.y;
+let specifiedLambda: (this: void, x: number) => number = x => x + 12;
+let specifiedLambdaToSpecified: (this: {y: number}, x: number) => number = specifiedLambda;
