40afcc49f5ed46016ab4c2577365bb5fb9e635fd
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-01-28 20:51:12 -0800

    Update baselines with improved types
    1. Type of Function.apply/bind/call improved.
    2. Quick info for `this` improved (mostly).

---
+++ tests/baselines/reference/thisTypeInFuncTemp.js
@@ -0,0 +1,11 @@
+//// [thisTypeInFuncTemp.ts]
+function f(this: { y: number } = { y: 1 }, x: number = 2) {
+    return x + this.y;
+}
+
+//// [thisTypeInFuncTemp.js]
+function f(_a, x) {
+    var _b = _a.y,  = _b === void 0 ? 1 : _b;
+    if (x === void 0) { x = 2; }
+    return x + this.y;
+}
